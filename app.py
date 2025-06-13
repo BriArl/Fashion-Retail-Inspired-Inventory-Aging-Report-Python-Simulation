e767d75e0f7a809b562ba0d388d2a8258093692e
BriArl <brianna.youte@gmail.com>
2025-06-12 22:54:50 -0400

    move the sample download outside the if uploaded_file: block so it’s visible before upload

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import streamlit as st
 
 st.title("Retail Inventory Aging Report")
 
+with open("Sample_inventory.csv", "rb") as f:
+        st.download_button(
+            label ="Download Sample Inventory CSV",
+            data=f,
+            file_name = "Sample_inventory.csv",
+            mime="text/csv"
+        )
+
 uploaded_file = st.file_uploader("Upload Inventory CSV", type="csv")
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
@@ -26,14 +34,6 @@ if uploaded_file:
 
     st.subheader("Inventory Aging Summary")
     st.dataframe(df[['material', 'description', 'category', 'store', 'quantity', 'days_old', 'aging_bucket']])
-
-    with open("Sample_inventory.csv", "rb") as f:
-        st.download_button(
-            label ="Download Sample Inventory CSV",
-            data=f,
-            file_name = "Sample_inventory.csv",
-            mime="text/csv"
-        )
 
     csv = df.to_csv(index=False).encode()
     st.download_button("Download", data=csv, file_name="aging_report.csv", mime='text/csv')
